24ed278ae3b7818fe5d050a96bdb579370908451
Tarcisio <35512873+tcortes55@users.noreply.github.com>
2024-07-04 01:14:45 +0200

    PWA

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 // script.js
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/service-worker.js')
-        .then((registration) => {
-          console.log('Service Worker registrado com sucesso:', registration);
-        })
-        .catch((error) => {
-          console.log('Falha ao registrar o Service Worker:', error);
-        });
-    });
-  }
+navigator.serviceWorker.register('/service-worker.js')
+.then((registration) => {
+    console.log('Service Worker registrado com sucesso:', registration);
+})
+.catch((error) => {
+    console.log('Falha ao registrar o Service Worker:', error);
+});
 
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('expense-form');
