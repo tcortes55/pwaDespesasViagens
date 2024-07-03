92f2d4f6cb850c96cd94d7753a3a0b20dfcbec12
Tarcisio <35512873+tcortes55@users.noreply.github.com>
2024-07-04 01:44:22 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -12,37 +12,41 @@ const urlsToCache = [
     '/icon-512x512.png'
 ];
 
-// Evento de instalação do service worker
 self.addEventListener('install', event => {
+    console.log('[Service Worker] Install');
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(cache => {
+                console.log('[Service Worker] Caching all: app shell and content');
                 return cache.addAll(urlsToCache);
             })
     );
 });
 
-// Evento de busca
 self.addEventListener('fetch', event => {
+    console.log('[Service Worker] Fetching', event.request.url);
     event.respondWith(
         caches.match(event.request)
             .then(response => {
                 if (response) {
+                    console.log('[Service Worker] Found in cache', event.request.url);
                     return response;
                 }
+                console.log('[Service Worker] Network request for', event.request.url);
                 return fetch(event.request);
             })
     );
 });
 
-// Evento de ativação
 self.addEventListener('activate', event => {
+    console.log('[Service Worker] Activate');
     const cacheWhitelist = [CACHE_NAME];
     event.waitUntil(
         caches.keys().then(cacheNames => {
             return Promise.all(
                 cacheNames.map(cacheName => {
                     if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        console.log('[Service Worker] Deleting old cache:', cacheName);
                         return caches.delete(cacheName);
                     }
                 })
